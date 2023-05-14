902451adb259e24a7315146e2be298c67ca6e529
ymkim <ymkim1085@funin.camp>
2023-05-14 15:29:55 +0900

    feat: HTTP POST JSON 처리 완료

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private String age;
+
+    /*public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }*/
+}
