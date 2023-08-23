4027d090602064341201c1fbd2202625e0a0aa37
ymkim <ymkim1085@funin.camp>
2023-08-23 23:59:48 +0900

    feat: 순수 서블릿을 통해서 회원 관리 앱 생성
    * 회원 기본 폼 반환 기능 구현
    * 회원 저장 기능 구현
    * 회원 전체 조회 기능 구현

---
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,56 @@
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @since           :       2023-08-23
+ * @author          :       youngmin
+ * @version         :       1.0.0
+ * @description     :       회원 정보를 저장하는 서블릿 클래스
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-08-23       youngmin           최초 생성
+ **/
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        Member savedMember = memberRepository.save(member);
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter w = response.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                "   <meta charset=\"UTF-8\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                "   <li>id="+member.getId()+"</li>\n" +
+                "   <li>username="+member.getUsername()+"</li>\n" +
+                "   <li>age="+member.getAge()+"</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+}
